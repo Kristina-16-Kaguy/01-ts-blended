ec24923f61b5c3b7f72c99a5fec35fadb28062b1
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2026-01-16 17:13:10 +0200

    commit task 7

--- src/task-7.ts
+++ src/task-7.ts
@@ -1,4 +1,11 @@
-const users = [
+interface User {
+  name: string;
+  age: number;
+}
+
+const users: User[] = [
   { name: "Alice", age: 30 },
   { name: "Bob", age: 25 },
 ];
+
+users.push({ name: "Oleksa", age: 20 });
