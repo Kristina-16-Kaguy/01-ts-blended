690d53f77782268fefc86bb199f2bd2578628cd6
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2026-01-16 23:15:44 +0200

    commit  task 19

--- src/task-20.ts
+++ src/task-20.ts
@@ -1,14 +1,31 @@
 // Функція fetchPosts повинна отримати список постів з API за допомогою бібліотеки axios.
 
-// import axios from "axios";
-
-// const fetchPosts = async () => {
-//   const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-//   return response.data;
-// };
-
 // Завдання:
 
 // Оголосіть інтерфейс Post для поста (перевірте, які властивості користувача містяться у відповіді бекенда).
 // Типізуйте функцію fetchPosts, вказавши, що вона повертає проміс, який містить масив об'єктів типу Post.
 // Оголосіть функцію logThreePosts, яка виведе в консоль дані перших 3 постів, виводячи їхні title та body.
+
+import axios from "axios";
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const fetchPosts = async (): Promise<Post[]> => {
+  const response = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return response.data;
+};
+
+function logThreePosts(posts: Post[]): void {
+  for (let i = 0; i < 3; i++) {
+    console.log(posts[i]);
+  }
+}
+
+logThreePosts();
