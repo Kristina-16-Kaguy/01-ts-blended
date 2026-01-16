1d860b35b6c392b899a0b811c03ff42c17a185f4
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2026-01-16 22:38:59 +0200

    commit task 15-17

--- src/task-17.ts
+++ src/task-17.ts
@@ -1,14 +1,41 @@
 // Функція fetchUsers повертає проміс, який через fetch отримує список користувачів з API.
 
-// const fetchUsers = async () => {
-//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//   const data = await response.json();
-//   return data;
-// };
-
-// fetchUsers().then((users) => console.log(users));
-
 // Завдання:
 
 // Оголосіть інтерфейс User для користувача (перевірте, які властивості користувача містяться у відповіді бекенда).
 // Типізуйте функцію fetchUsers.
+
+interface Adress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  adress: Adress;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = (await response.json()) as User[];
+  return data;
+};
+
+fetchUsers().then((users) => console.log(users));
